e2a0d7d5462dfc4c44a3bae7f64011a1d60837a3
maylisha <jeonhana85@gmail.com>
2025-09-02 09:06:29 +0700

    upload apk
    ini

---
+++ BelajarCrud/resources/views/create.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h4>Tambah Data Siswa</h4>
+            </div>
+            <div class="card-body">
+                <form method="post" action="{{route('store')}}">
+                    @csrf
+                    @method('post')
+                    <div class="mb-3">
+                      <label for="nama" class="form-label">Nama : </label>
+                      <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+                    <div class="mb-3">
+                      <label for="nisn" class="form-label">NISN : </label>
+                      <input type="text" class="form-control" id="nisn" name="nisn">
+                    </div>
+                    <div class="mb-3">
+                        <label for="jurusan" class="form-label">Jurusan : </label>
+                        <input type="text" class="form-control" id="jurusan" name="jurusan">
+                      </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <a href="/siswa" class="btn btn-secondary">Kembali</a>
+                  </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
